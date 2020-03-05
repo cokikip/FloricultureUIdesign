fc997bdd71272fc407693957ff5263437aefe980
cokikipla <cokicollinji@gmail.com>
2020-03-05 12:18:11 +0300

    FloricultureUIdesign

---
+++ new.js
@@ -0,0 +1,15 @@
+jQuery.ajaxSetup({
+  url: '/path/to/file',
+  type: 'POST',
+  dataType: 'xml/html/script/json/jsonp',
+  data: {param1: 'value1'},
+  complete: function(xhr, textStatus) {
+    //called when complete
+  },
+  success: function(data, textStatus, xhr) {
+    //called when successful
+  },
+  error: function(xhr, textStatus, errorThrown) {
+    //called when there is an error
+  }
+});
